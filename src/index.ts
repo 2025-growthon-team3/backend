d7767a24b92ede24dcbbd5a5ce9e61cdb758808c
brightree <treebright9@gmail.com>
2025-05-17 21:59:31 +0900

    [#10] feat: 카카오 로그인 기능 개발

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,28 @@
-import "reflect-metadata";
+console.log("index.ts 실행됨");
+
 import express from "express";
-import { AppDataSource } from "./data-source";
-import { User } from "./entity/User";
 import cors from "cors";
+import authRouter from "./routes/auth.route";
+import { AppDataSource } from "./data-source";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/auth", authRouter);
+
 AppDataSource.initialize()
   .then(() => {
     console.log("DB connected");
-
-    app.get("/users", async (req, res) => {
-      const users = await AppDataSource.getRepository(User).find();
-      res.json(users);
-    });
-
-    app.post("/users", async (req, res) => {
-      const { name, email } = req.body;
-      const user = AppDataSource.getRepository(User).create({ name, email });
-      const result = await AppDataSource.getRepository(User).save(user);
-      res.json(result);
-    });
-
-    app.listen(3000, () => {
-      console.log("Server running at http://localhost:3000");
-    });
   })
-  .catch((error) => console.error(error));
+  .catch((error) => {
+    console.error("❌ DB 연결 실패:", error);
+  });
+
+app.listen(3000, () => {
+  console.log("서버 실행 중: http://localhost:3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("서버가 살아 있습니다");
+});
